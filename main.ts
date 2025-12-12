c367f2b17160883a042c05faceed7f964829b12c
Aarav Singhal <aarav.singhal@stu.ocsb.ca>
2025-12-11 20:31:46 -0500

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,42 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2025 All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Aarav Singhal
+ * Created on: Dec 2025
+ * This program uses the bluetooth radios
 */
 
-basic.showString('Hello, World!')
+// variables
+let distanceToObject: number = 0
+
+// setup
+radio.setGroup(247)
+basic.showIcon(IconNames.Happy)
+
+// receive
+radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    basic.showString(receivedString)
+    basic.showIcon(IconNames.Happy)
+})
+
+// loop
+while (true) {
+    distanceToObject = sonar.ping(
+        DigitalPin.P12,
+        DigitalPin.P13,
+        PingUnit.Centimeters
+    )
+
+    // if distance is less than 5
+    if (distanceToObject < 5) {
+        basic.clearScreen()
+        basic.showNumber(distanceToObject)
+        radio.sendString("Too close")
+        basic.pause(1000)
+    } else {
+        basic.clearScreen()
+        basic.showNumber(distanceToObject)
+        radio.sendString("")
+        basic.pause(1000)
+    }
+}
